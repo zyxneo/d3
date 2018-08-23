8167e0e0144f68e192b51a1356817392bb3d6761
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2018-08-23 07:55:36 +0200

    Updated SVG vertical bar chart

--- src/pages/svg-bar-chart-vertical.jsx
+++ src/pages/svg-bar-chart-vertical.jsx
@@ -23,22 +23,23 @@ class IndexPage extends React.Component<Props, State> {
 
   state: State
 
-  constructor(props: Props) {
-    super(props)
-    this.state = {}
-  }
-
   componentDidMount() {
     this.updateBar()
   }
 
   componentDidUpdate() {
-    this.updateBar()
+    // this.updateBar() // cause multiple "select" and "append", ends with repeating svg nodes
   }
 
   updateBar = () => {
-    const width = 960
-    const height = 500
+    const margin = {
+      top: 20,
+      right: 30,
+      bottom: 30,
+      left: 40,
+    }
+    const width = 960 - margin.left - margin.right
+    const height = 500 - margin.top - margin.bottom
 
     // https://github.com/d3/d3-scale
 
@@ -52,16 +53,19 @@ class IndexPage extends React.Component<Props, State> {
       .range([height, 0])
 
     const chart = d3.select('.chart')
-      .attr('width', width)
-      .attr('height', height)
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
     d3.tsv(withPrefix('/db/data-vertical.tsv')).then((data) => {
       x.domain(data.map(d => d.letter))
       y.domain([0, Math.max(...data.map(d => d.frequency))])
 
-      const bar = chart.selectAll('g')
+      const bar = chart.selectAll('g.column')
         .data(data)
         .enter().append('g')
+        .attr('class', '.column') // do not forget to identify selection!
         .attr('transform', d => `translate(${x(d.letter)},0)`)
 
       bar.append('rect')
@@ -74,8 +78,25 @@ class IndexPage extends React.Component<Props, State> {
         .attr('dy', '.35em')
         /* eslint-disable no-confusing-arrow */
         .attr('dx', d => (y(d.frequency) >= height / 2) ? '-1em' : '4em')
+        .attr('class', d => (y(d.frequency) >= height / 2) ? 'upper' : 'lower')
+        /* eslint-enable no-confusing-arrow */
         .attr('transform', d => `rotate(90), translate(${y(d.frequency)}, -${x.bandwidth() / 2})`)
         .text(d => d.frequency)
+
+      const xAxis = d3.axisBottom(x)
+        .tickValues(data.map(d => d.letter))
+
+      chart.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', `translate(0, ${height})`)
+        .call(xAxis)
+
+      const yAxis = d3.axisLeft(y)
+        .ticks(10)
+
+      chart.append('g')
+        .attr('class', 'y axis')
+        .call(yAxis)
     })
   }
 
