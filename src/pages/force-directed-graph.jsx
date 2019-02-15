4678419b2a4b287c951939440c26e77cc8fdb9fb
Balázs Szilágyi <balazs.szilagyi@radarservices.com>
2019-02-15 18:34:31 +0100

    Update force-tutorial with selectable grab

--- src/pages/force-directed-graph.jsx
+++ src/pages/force-directed-graph.jsx
@@ -43,10 +43,13 @@ class IndexPage extends React.Component<Props> {
       height,
     } = canvas
 
+    let selectedNode
+
     const simulation = d3.forceSimulation()
       .force('link', d3.forceLink().id(d => d.id))
       .force('charge', d3.forceManyBody())
-      .force('center', d3.forceCenter())
+      .force('center', d3.forceCenter(width / 2, height / 2))
+
 
     d3.json(withPrefix('/db/miserables.json')).then((graph, error) => {
       if (error) throw error
@@ -61,17 +64,48 @@ class IndexPage extends React.Component<Props> {
         const maxWidth = 30
         const textPadding = 6
 
+        context.save()
+        context.beginPath()
+        context.lineWidth = 2
+        context.strokeStyle = '#fff'
+        context.setLineDash([])
+        context.stroke()
         context.moveTo(d.x + rad, d.y)
         context.arc(d.x, d.y, rad, 0, 2 * Math.PI)
 
         context.font = '10px sans-serif'
         context.fillText(d.id, d.x + rad + textPadding, d.y + rad, maxWidth)
+        context.fillStyle = selectedNode === d ? 'red' : '#333'
+        context.fill()
+        context.stroke()
+        context.restore()
+      }
+
+      function dragstarted() {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+        d3.event.subject.fx = d3.event.subject.x
+        d3.event.subject.fy = d3.event.subject.y
+      }
+
+      function dragged() {
+        d3.event.subject.fx = d3.event.x
+        d3.event.subject.fy = d3.event.y
+      }
+
+      function dragended() {
+        if (!d3.event.active) simulation.alphaTarget(0)
+        d3.event.subject.fx = null
+        d3.event.subject.fy = null
+        selectedNode = d3.event.subject
+      }
+
+      function dragsubject() {
+        return simulation.find(d3.event.x, d3.event.y)
       }
 
       function ticked() {
         context.clearRect(0, 0, width, height)
         context.save()
-        context.translate(width / 2, height / 2 + 40)
 
         context.beginPath()
         graph.links.forEach(drawLink)
@@ -80,22 +114,26 @@ class IndexPage extends React.Component<Props> {
         context.setLineDash([2])
         context.stroke()
 
-        context.beginPath()
         graph.nodes.forEach(drawNode)
-        context.fill()
-        context.lineWidth = 2
-        context.strokeStyle = '#fff'
-        context.setLineDash([])
-        context.stroke()
 
         context.restore()
       }
+
       simulation
         .nodes(graph.nodes)
         .on('tick', ticked)
 
-      simulation.force('link')
+      simulation
+        .force('link')
         .links(graph.links)
+
+      d3.select(canvas)
+        .call(d3.drag()
+          .container(canvas)
+          .subject(dragsubject)
+          .on('start', dragstarted)
+          .on('drag', dragged)
+          .on('end', dragended))
     })
   }
 
